eb01aa490878721963dfac1c03aafa41c87d9468
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-05-12 17:27:14 -0300

    asd

---
+++ taller/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields =('id','vehiculo','dueño','falla','reparado','fecha')  
+        read_only_fields =('id' ,'fecha')  
+        
