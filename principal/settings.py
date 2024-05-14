4059773e21be4c1c363e96069fa0c40540bf2528
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-05-14 19:08:35 -0300

    preparando para deploy4

--- principal/settings.py
+++ principal/settings.py
@@ -125,4 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CORS_ALLOWED_ORIGINS = [
+    
+   'localhost','https://vercel.com/alejandros-projects-30c19e38/taller-frontend',
+   
+]
