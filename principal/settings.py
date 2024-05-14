d210b5cecd02480cd716c773b71331eb0dee7928
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-05-14 18:41:33 -0300

    preparando para deploy3

--- principal/settings.py
+++ principal/settings.py
@@ -125,8 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    
-    "http://localhost:3000",
-   
-]
+
