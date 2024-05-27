2762ce236a4f5a357f1fd0d4cc0a1051b0d7a8f4
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-05-27 19:29:35 -0300

    cambiando direccion de cors_allow5

--- principal/settings.py
+++ principal/settings.py
@@ -127,6 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     
-    "https://taller-frontend-4y9r1knhx-alejandros-projects-30c19e38.vercel.app/",
+    "https://taller-frontend-hpukmhm1l-alejandros-projects-30c19e38.vercel.app/",
    
 ]
